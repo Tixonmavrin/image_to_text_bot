2314cf28f03ca2bf0d659f7651905ed67be1a26c
Tikhon Mavrin <tixonmavrin@gmail.com>
2021-04-27 20:47:18 +0300

    Add main testing file

---
+++ test_all.py
@@ -0,0 +1,9 @@
+# Import library for testing our program
+import unittest
+
+# Get tests from tests folder
+from tests import *
+
+if __name__ == "__main__":
+    # Start testing
+    unittest.main()
