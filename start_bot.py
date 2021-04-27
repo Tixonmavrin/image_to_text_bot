3ab8ca567c4ee19f1769b68255c53ee04d7763aa
Tikhon Mavrin <tixonmavrin@gmail.com>
2021-04-27 20:46:52 +0300

    Add start file

---
+++ start_bot.py
@@ -0,0 +1,19 @@
+# Library for command line arguments parsing
+import argparse
+
+# Import our library
+import image_to_text_bot
+
+if __name__ == "__main__":
+    # Parse arguments (keys for telegram and google cloud)
+    parser = argparse.ArgumentParser(description="Image-to-text telegram bot")
+    parser.add_argument(
+        "--telegram_key", type=str, default="", help="Telegram key from BotFather"
+    )
+    parser.add_argument(
+        "--google_keys_json_path", type=str, default="google_keys.json", help="Path to json file from google cload vision api"
+    )
+    args = parser.parse_args()
+
+    # Start the bot, add google vision api connection
+    image_to_text_bot.start(args.__dict__)
